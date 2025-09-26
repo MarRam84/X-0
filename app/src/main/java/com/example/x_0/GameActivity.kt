c36d18432d6e1863fdf116f805709a228de28c32
Marlon Miranda <marram0312@gmail.com>
2025-09-25 23:31:05 -0600

    validacion

--- app/src/main/java/com/example/x_0/GameActivity.kt
+++ app/src/main/java/com/example/x_0/GameActivity.kt
@@ -67,18 +67,38 @@ class GameActivity : AppCompatActivity() {
         if(!gameFinished && mybtn.text.toString().isEmpty()){
             if(currentPlayer == 1){
                 mybtn.text = "X"
-                validateWinner()
+                validateWinner(btn)
                 currentPlayer = 2
             }
             else{
                 mybtn.text = "O"
-                validateWinner()
+                validateWinner(btn)
                 currentPlayer = 1
             }
 
         }
     }
-    fun validateWinner(){
+    fun validateWinner(btn: View){
+
+
+        if (validateCards(btn)){
+            if (currentPlayer == 1) {
+                scorePlayer1++
+                tvscoreplayer1.text = "$scorePlayer1"
+                Toast.makeText(applicationContext, "${tvPlayer1.text} Ha Ganado", Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                scorePlayer2++
+                tvscoreplayer2.text = "$scorePlayer2"
+                Toast.makeText(applicationContext, "${tvPlayer2.text} Ha Ganado", Toast.LENGTH_LONG)
+                    .show()
+            }
+            gameFinished = true
+        }
+
+    }
+
+    private fun validateCards(btn: View): Boolean {
         var bt1Val = bt1.text.toString().trim()
         var bt2Val = bt2.text.toString().trim()
         var bt3Val = bt3.text.toString().trim()
@@ -88,34 +108,97 @@ class GameActivity : AppCompatActivity() {
         var bt7Val = bt7.text.toString().trim()
         var bt8Val = bt8.text.toString().trim()
         var bt9Val = bt9.text.toString().trim()
-        val bool = if (!bt1Val.isEmpty() && bt1Val.equals(bt2Val) && bt1Val.equals(bt3Val))
+        var winner = false
+
+        when(btn.id){
+            bt1.id->{
+                if (!bt1Val.isEmpty() &&
+                (bt1Val.equals(bt2Val) && bt1Val.equals(bt3Val))
                 ||
-                (!bt1Val.isEmpty() && bt1Val.equals(bt2Val) && bt1Val.equals(bt3Val)){
-                    if (currentPlayer == 1) {
-                        scorePlayer1++
-                        tvscoreplayer1.text = "$scorePlayer1"
-                        Toast.makeText(
-                            applicationContext,
-                            "${tvPlayer1.text} Ha Ganado",
-                            Toast.LENGTH_LONG
-                        )
-                            .show()
-                    } else {
-                        scorePlayer2++
-                        tvscoreplayer2.text = "$scorePlayer2"
-                        Toast.makeText(
-                            applicationContext,
-                            "${tvPlayer2.text} Ha Ganado",
-                            Toast.LENGTH_LONG
-                        )
-                            .show()
-                    }
-                    gameFinished = true
+                (bt1Val.equals(bt5Val) && bt1Val.equals(bt9Val))
+                ||
+                (bt1Val.equals(bt4Val) && bt1Val.equals(bt7Val))) {
+                    winner = true
+                }
+            }
+            bt2.id ->{
+                if (!bt2Val.isEmpty() &&
+                    (bt2Val.equals(bt1Val) && bt2Val.equals(bt3Val))
+                    ||
+                    (bt2Val.equals(bt5Val) && bt1Val.equals(bt7Val))){
+                    winner = true
+                }
+            }
+            bt3.id -> {
+                if (!bt3Val.isEmpty() &&
+                    (bt3Val.equals(bt1Val) && bt3Val.equals(bt2Val))
+                    ||
+                    (bt3Val.equals(bt6Val) && bt3Val.equals(bt9Val))
+                    ||
+                    (bt3Val.equals(bt5Val) && bt3Val.equals(bt7Val))) {
+                    winner = true
+                }
+            }
+            bt4.id -> {
+                if (!bt4Val.isEmpty() &&
+                    (bt4Val.equals(bt1Val) && bt4Val.equals(bt7Val))
+                    ||
+                    (bt4Val.equals(bt5Val) && bt4Val.equals(bt6Val))) {
+                    winner = true
+                }
+            }
+            bt5.id -> {
+                if (!bt5Val.isEmpty() &&
+                    (bt5Val.equals(bt2Val) && bt5Val.equals(bt8Val))
+                    ||
+                    (bt5Val.equals(bt4Val) && bt5Val.equals(bt6Val))
+                    ||
+                    (bt5Val.equals(bt1Val) && bt5Val.equals(bt9Val))
+                    ||
+                    (bt5Val.equals(bt3Val) && bt5Val.equals(bt7Val))) {
+                    winner = true
+                }
+            }
+            bt6.id -> {
+                if (!bt6Val.isEmpty() &&
+                    (bt6Val.equals(bt3Val) && bt6Val.equals(bt9Val))
+                    ||
+                    (bt6Val.equals(bt4Val) && bt5Val.equals(bt5Val))) {
+                    winner = true
+                }
+            }
+            bt7.id -> {
+                if (!bt7Val.isEmpty() &&
+                    (bt7Val.equals(bt1Val) && bt7Val.equals(bt4Val))
+                    ||
+                    (bt7Val.equals(bt5Val) && bt7Val.equals(bt3Val))
+                    ||
+                    (bt7Val.equals(bt8Val) && bt7Val.equals(bt9Val))){
+                    winner = true
+                }
+            }
+            bt8.id -> {
+                if (!bt8Val.isEmpty() &&
+                    (bt8Val.equals(bt7Val) && bt8Val.equals(bt9Val))
+                    ||
+                    (bt8Val.equals(bt2Val) && bt8Val.equals(bt5Val))) {
+                    winner = true
+                }
+            } bt9.id -> {
+                if (!bt9Val.isEmpty() &&
+                    (bt9Val.equals(bt3Val) && bt9Val.equals(bt6Val))
+                    ||
+                    (bt9Val.equals(bt5Val) && bt9Val.equals(bt1Val))
+                    ||
+                    (bt9Val.equals(bt7Val) && bt9Val.equals(bt8Val))) {
+                    winner = true
+                }
+            }
+            else->{
+                    winner = false
                 }
 
-    }
-
-    private fun validateCards(): Boolean {
-        TODO("Not yet implemented")
+        }
+        return winner
     }
 }
